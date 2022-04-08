0eea732db31a1fa747de22abad6f7b294d4cf929
cancio <filipe.cancio@outlook.com>
2022-04-08 20:07:55 -0300

    Novo app em Jetpack Compose #1

---
+++ mmcompose/app/src/main/java/dev/cancio/mm_compose/MainActivity.kt
@@ -0,0 +1,43 @@
+package dev.cancio.mm_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.cancio.mm_compose.ui.theme.MmcomposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MmcomposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MmcomposeTheme {
+        Greeting("Android")
+    }
+}
