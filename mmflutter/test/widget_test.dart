9203449429ea71aaab46ee94b6d1b0cead7f9101
cancio <filipe.cancio@outlook.com>
2022-04-10 19:21:43 -0300

    update package

--- mmflutter/test/widget_test.dart
+++ mmflutter/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mm_flutter/main.dart';
+import 'package:mmflutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
