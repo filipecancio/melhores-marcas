b621419cae04974d04cb92dc51d26de46af2d124
cancio <filipe.cancio@outlook.com>
2022-04-17 11:49:52 -0300

    criando tela home

---
+++ mmflutter/lib/ds/colors.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //Coal Colors
+  static final coal_1 = Color(0xFFEFEFEF);
+  static final coal_2 = Color(0xFFCFCFCF);
+  static final coal_3 = Color(0xFF8F908F);
+  static final coal_4 = Color(0xFF50514F);
+  static final coal_5 = Color(0xFF333433);
+  static final coal_6 = Color(0xFF161716);
+  //River Colors
+  static final river_1 = Color(0xFFD2FAF9);
+  static final river_2 = Color(0xFFA5F6F3);
+  static final river_3 = Color(0xFF61F0EA);
+  static final river_4 = Color(0xFF08E8DE);
+  static final river_5 = Color(0xFF06948E);
+  static final river_6 = Color(0xFF03403D);
+  //Ocean Colors
+  static final ocean_1 = Color(0xFFD5E9F5);
+  static final ocean_2 = Color(0xFFACD4EC);
+  static final ocean_3 = Color(0xFF6FB4DD);
+  static final ocean_4 = Color(0xFF1D8ACB);
+  static final ocean_5 = Color(0xFF166594);
+  static final ocean_6 = Color(0xFF0E3F5D);
+}
---
+++ mmflutter/lib/ui/modules/home/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text("Melhores Marcas"),
+            ]),
+      ),
+    );
+  }
+}
